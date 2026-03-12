fe33d2d1088649ec99e7e199fd11ee64c75d6dac
VELİ DOĞAN <velidogan120@gmail.com>
2026-03-12 03:47:40 +0300

    feat: update useSkills and useProjects hooks to accept config parameter for enhanced flexibility

--- src/lib/services/mockApiQuery.js
+++ src/lib/services/mockApiQuery.js
@@ -1,24 +1,26 @@
 import { useQuery } from "@tanstack/react-query";
 import { getMocksApi, getGitApi } from "../api/getAxios";
 
-export const useSkills = (language, params = {}) => {
+export const useSkills = (language, config = {}) => {
   return useQuery({
     queryKey: ["skills", language],
     queryFn: async () => {
       const { data } = await getMocksApi.get("/skills", {
-        params: { ...params, lang: language },
+        ...config,
+        params: { lang: language },
       });
       return data;
     },
   });
 };
 
-export const useProjects = (language, params = {}) => {
+export const useProjects = (language, config = {}) => {
   return useQuery({
     queryKey: ["projects", language],
     queryFn: async () => {
       const { data } = await getGitApi.get("/projects", {
-        params: { ...params, lang: language },
+        ...config,
+        params: { lang: language },
       });
       return data;
     },
