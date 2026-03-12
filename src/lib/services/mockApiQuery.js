86625248d65fd847c823d78709bed7b098752f3a
VELİ DOĞAN <velidogan120@gmail.com>
2026-03-12 03:41:02 +0300

    feat: enhance useSkills and useProjects hooks to accept additional query parameters

--- src/lib/services/mockApiQuery.js
+++ src/lib/services/mockApiQuery.js
@@ -1,24 +1,24 @@
 import { useQuery } from "@tanstack/react-query";
 import { getMocksApi, getGitApi } from "../api/getAxios";
 
-export const useSkills = (language) => {
+export const useSkills = (language, params = {}) => {
   return useQuery({
     queryKey: ["skills", language],
     queryFn: async () => {
       const { data } = await getMocksApi.get("/skills", {
-        params: { lang: language },
+        params: { ...params, lang: language },
       });
       return data;
     },
   });
 };
 
-export const useProjects = (language) => {
+export const useProjects = (language, params = {}) => {
   return useQuery({
     queryKey: ["projects", language],
     queryFn: async () => {
       const { data } = await getGitApi.get("/projects", {
-        params: { lang: language },
+        params: { ...params, lang: language },
       });
       return data;
     },
